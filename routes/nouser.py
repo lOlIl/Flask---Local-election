f4d4057ac2d420bd4762f5091cc8829c6eba92cb
Peter Margetiak <peter.margetiak@gmail.com>
2011-06-05 18:32:49 +0200

    Basic web layout

--- routes/nouser.py
+++ routes/nouser.py
@@ -1,5 +1,10 @@
 from app import app
 
+from flask import render_template, flash
+
 @app.route('/')
 def index():
-    return "Simple test!"
+    flash(u'Test info message',category = 'info')
+    flash(u'Test warning message',category = 'warning')
+    flash(u'Test error message',category = 'error')
+    return render_template('index.html')
