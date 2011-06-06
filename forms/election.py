927decd4672da006ed596afdedefca75a123608a
Peter Margetiak <peter.margetiak@gmail.com>
2011-06-06 15:33:51 +0200

    Election forms fix

---
+++ forms/election.py
@@ -0,0 +1,33 @@
+class ElectionNameField(object):
+    label=u"Name"        
+    name=u"name"
+
+class ElectionDescField(object):
+    label=u"Description"        
+    name=u"desc" 
+
+class ElectionShowField(object):
+    label=u"Activate"
+    name=u"show"
+
+class ElectionStartField(object):
+    label=u"Date start"
+    name=u"start"
+
+class ElectionEndField(object):
+    label=u"Date end"
+    name=u"end"
+
+class ElectionStartTimeField(object):
+    label=u"Time start"
+    name_1=u"startTime_hod"  
+    name_2=u"startTime_min"   
+    value_1=0
+    value_2=0   
+
+class ElectionEndTimeField(object):
+    label=u"Time end"
+    name_1=u"endTime_hod"
+    name_2=u"endTime_min"      
+    value_1=0
+    value_2=0   
