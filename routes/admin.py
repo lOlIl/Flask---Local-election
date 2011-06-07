e9bdd1d3f35941c3c74dcfc8b4e2887c50ef9d62
Peter Margetiak <peter.margetiak@gmail.com>
2011-06-07 12:09:52 +0200

    Added election update function

--- routes/admin.py
+++ routes/admin.py
@@ -22,6 +22,7 @@ from datetime import datetime
 
 INFO_DICT = {
             'ELECTION_ADDED'    : u'Created new election.',
+            'ELECTION_UPDATED'  : u'Election has been successfully updated.'
             }
 
 ERROR_DICT = {
@@ -80,3 +81,79 @@ def admin_election():
                  ])
     all_elections = Election.query.all()
     return render_template('admin/election/main.html', input = inputs, elections = all_elections)
+
+"""
+
+    Admin function for updating election
+    Inputs: id_election(election ID)
+
+    Description:
+    - show selected election in form
+
+    on POST: update the election fields, if there are correct inputs.
+    
+"""
+
+@app.route('/admin/election/<int:id_election>/update', methods=['GET', 'POST'])
+@admin_required
+def admin_election_update(id_election):
+    election = Election.query.filter_by(id = id_election).first()
+    if election:
+        if request.method == 'POST':    
+            election.title =    unicode(request.form['name'])
+            election.text =     unicode(request.form['desc'])
+
+            if request.form.get('show') and request.form['show'] == 'checked':
+                election.show = True
+            else:
+                election.show = False
+            
+            try:
+                election.date_from = datetime.strptime(
+                                        unicode(request.form['start'])+' '+\
+                                        unicode(request.form['startTime_hod'])+':'+\
+                                        unicode(request.form['startTime_min'])+':00',
+                                        '%Y-%m-%d %H:%M:%S')    
+                election.date_to = datetime.strptime(
+                                        unicode(request.form['end'])+' '+\
+                                        unicode(request.form['endTime_hod'])+':'+\
+                                        unicode(request.form['endTime_min'])+':00',
+                                        '%Y-%m-%d %H:%M:%S')          
+                db.session.commit()        
+                flash(INFO_DICT['ELECTION_UPDATED'])     
+            except:
+                flash(ERROR_DICT['INPUTS'])
+            return redirect(url_for('admin_election'))
+
+        inputs = dict([ 
+                    ('name',        ElectionNameField()),
+                    ('desc',        ElectionDescField()),
+                    ('start',       ElectionStartField()),
+                    ('startTime',   ElectionStartTimeField()),
+                    ('end',         ElectionEndField()),
+                    ('endTime',     ElectionEndTimeField()),
+                    ('show',        ElectionShowField())
+                 ])
+
+        inputs['name'].value = election.title
+        inputs['desc'].value = election.text
+
+        if election.show:
+            inputs['show'].checked = "yes"
+
+        x = election.date_from
+        inputs['start'].value = x.strftime('%Y-%m-%d')
+        inputs['startTime'].value_1 = atoi(x.strftime('%H'))
+        inputs['startTime'].value_2 = atoi(x.strftime('%M'))
+
+        x = election.date_to
+        inputs['end'].value = x.strftime('%Y-%m-%d')
+        inputs['endTime'].value_1 = atoi(x.strftime('%H'))
+        inputs['endTime'].value_2 = atoi(x.strftime('%M'))
+
+        all_elections = Election.query.all()
+
+        return render_template('admin/election/main.html', elections = all_elections, input = inputs, id_election = election.id)
+
+    flash(ELECTION_NONE,category = 'error')
+    return redirect(url_for('admin_election'))
